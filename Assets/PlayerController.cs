ef05f7d8232496eb856172b44553b73377b42c74
fumiaki1108 <pasocon4649@gmal.co.jp>
2020-12-29 02:41:49 +0900

    PlayerController

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private void FixedUpdate()
+    {
+       // Rigidbodyを取得
+       Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+       // 方向キーの入力
+       float x = Input.GetAxis("Horizontal"); // 横軸
+       float z = Input.GetAxis("Vertical");   // 縦軸
+
+       // ベクトルに変換
+       Vector3 vec = new Vector3(x, 0, z);
+
+       // 力を加える
+       rigidbody.AddForce(vec);
+    }
+
+}
